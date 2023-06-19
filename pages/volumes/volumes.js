10d22d4618e1bd6e9e0bad36841237fbaa6fdca9
Nils Otto <nilsotto@gmx.de>
2023-06-19 11:22:18 +0200

    init commit

---
+++ pages/volumes/volumes.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { introduction } from "../../lib/data";
+
+export function Volumes() {
+  return (
+    <>
+      <h1>Lord of the Rings</h1>
+      <Link href="/"></Link>
+    </>
+  );
+}
